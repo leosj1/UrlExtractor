09a516361639eb58c53f849205d71a1136ac7322
seun <johnsonseun15@gmail.com>
2020-06-23 10:54:00 -0700

    changes
    Co-authored-by: jkready@ualr.edu <jkready@ualr.edu>

--- UrlExtractor/spiders/burrardstreetjournal.py
+++ UrlExtractor/spiders/burrardstreetjournal.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from UrlExtractor.items import Posts
-from UrlExtractor.utils import *
+from UrlExtractor.utils import relevant
 
 class BurrardstreetjournalSpider(scrapy.Spider):
     name = 'burrardstreetjournal'
     allowed_domains = ['burrardstreetjournal.com']
     domain = 'burrardstreetjournal.com'
-    start_urls = ['https://www.burrardstreetjournal.com/?s=corona']
+    start_urls = ['https://www.burrardstreetjournal.com']
 
     def parse(self, response):
-        all_urls = response.css('.td_module_16 .td-module-title a').xpath('@href').extract()
+        all_categories = response.css('#menu-main-menu-1 a').xpath('@href').extract()
+        if response.url == 'https://www.burrardstreetjournal.com':
+            for link in all_categories:
+                yield scrapy.Request(link)
+
+        all_urls = response.css('.td-module-title a').xpath('@href').extract()
         for url in all_urls:
-            yield scrapy.Request( url, self.parse_blog)
+            yield scrapy.Request(url, self.parse_blog)
 
         next_page = response.css('.last+ a').xpath('@href').extract_first()
         if next_page: yield scrapy.Request(next_page, self.parse)
@@ -22,4 +27,6 @@ class BurrardstreetjournalSpider(scrapy.Spider):
         blog = Posts()
         blog['domain'] = self.domain
         blog['url'] = response.url
-        yield blog
+        content = " ".join(response.css('.td-post-content').xpath('//text()').extract()).replace('\n','').replace('\t','').replace('\r','')
+        if relevant(content, use='ausi_covid') or relevant(content, use='ausi_dod'):
+            yield blog
--- UrlExtractor/spiders/unz.py
+++ UrlExtractor/spiders/unz.py
@@ -1,25 +1,37 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from UrlExtractor.items import Posts
-
+from UrlExtractor.utils import relevant
 
 class UnzSpider(scrapy.Spider):
     name = 'unz'
     allowed_domains = ['unz.com']
     domain = 'unz.com'
-    start_urls = ['https://www.unz.com/?s=covid-19&Action=Search&ptype=all&paged=1']
-    # next_page = 2
+    
+    # start_urls = ['https://www.unz.com/?s=covid-19&Action=Search&ptype=all&paged=1']
+    start_urls = ['https://www.unz.com/']
 
     def parse(self, response):
-        all_urls = response.xpath('//*[@id="wrap-content"]/div/div[1]/a/@href').extract()
+        all_categories = response.xpath('//*[@id="topmenu-holder"]/div/ul[1]/div/li/a/@href').extract()
+        if response.url == 'https://www.unz.com/':
+            for link in all_categories:
+                if link != '/' and link != '/masthead/':
+                    if 'tcategory' in link:
+                        link = link.replace('tcategory', 'category')
+                        yield scrapy.Request('https://www.unz.com' + link)
+
+        all_urls = response.css('#contents-holder .head a').xpath('@href').extract()
         for url in all_urls:
-            blog = Posts()
-            blog['domain'] = self.domain
-            blog['url'] = 'https://www.unz.com' + url
+            yield scrapy.Request('https://www.unz.com' + url, self.parse_blog)
+        
+        next_page = response.css('.leftside a').xpath('@href').extract_first()
+        if next_page: yield scrapy.Request('https://www.unz.com' + next_page, self.parse)
+
+    def parse_blog(self, response):
+        # Posts
+        blog = Posts()
+        blog['domain'] = self.domain
+        blog['url'] = response.url
+        content = " ".join(response.xpath('//*[@id="contents-holder"]//text()').extract()).replace('\n','').replace('\t','').replace('\r','')
+        if relevant(content, use='ausi_covid') or relevant(content, use='ausi_dod'):
             yield blog
-            # yield scrapy.Request(url, self.parse_blog)
-
-        page_number = response.url.split('paged=')[-1]
-        next_page = f"{''.join(response.url.split('paged=')[:-1])}paged={int(page_number) + 1}"
-        if next_page: yield scrapy.Request(next_page, self.parse)
-
