431591791403f9849c77baa3fa4d4d87a9427897
seun <johnsonseun15@gmail.com>
2020-07-14 12:58:25 -0600

    updates

--- UrlExtractor/spiders/google.py
+++ UrlExtractor/spiders/google.py
@@ -38,76 +38,82 @@ def get_google_request(words, project):
     api_keys = [x for x in api_keys if x != '']
 
     domains = get_txt_data('domains')
-    domain = "wordpress.com"
+    domains = [x for x in domains if x != '']
+    # domain = "wordpress.com"
+    # domain = domains[0]
 
-    start_index, start_status = get_start_page(domain, project, words)
-    start = -1 if start_index == -1 else start_index + 10
+    for domain in domains:
+        start_index, start_status = get_start_page(domain, project, words)
+        start = -1 if start_index == -1 else start_index + 10
 
-    # location = "Ukraine"
-    siteSearchFilter = "i"
-    cse_key = "008008521178205893081:w7ksepj1jro"
-    pbar = tqdm(total=len(api_keys), desc="Google Web Requests")
+        # location = "Ukraine"
+        siteSearchFilter = "i"
+        cse_key = "008008521178205893081:w7ksepj1jro"
+        pbar = tqdm(total=len(api_keys), desc="Google Web Requests")
 
-    required_keywords , or_keywords = words
-    required_keywords = urllib.parse.quote(required_keywords, safe='')
-    or_keywords = urllib.parse.quote(or_keywords, safe='')
+        required_keywords , or_keywords = words
+        required_keywords = urllib.parse.quote(required_keywords, safe='')
+        or_keywords = urllib.parse.quote(or_keywords, safe='')
 
-    limit = 100
-    today = datetime.datetime.now() #today's date
-    
-    while True:
-        if start == -1:
-            break
-
-        # page_url = f"https://www.googleapis.com/customsearch/v1?key={api_keys[0]}&start={start_index}&cx={cse_key}&cr={location}&siteSearch={domain}&siteSearchFilter={siteSearchFilter}&exactTerms={required_keywords}&orTerms={or_keywords}&as_occt=body"
-        page_url = f"https://www.googleapis.com/customsearch/v1?key={api_keys[0]}&start={start_index}&cx={cse_key}&siteSearch={domain}&siteSearchFilter={siteSearchFilter}&exactTerms={required_keywords}&orTerms={or_keywords}&as_occt=body"
-        # Dont send request if start_status is "NOT_FOUND"
-        if start_status == 'NOT_FOUND' or start <= limit - 1:
-            response = requests.get(page_url)
-            if response.status_code == 200:
-                response = json.loads(response.text)
-                if 'items' in response:
-                    results += response['items']
-
-                    if 'nextPage' in response['queries']:
-                        # Limiting to top 11 results
-                        next_index = response['queries']['nextPage'][0]['startIndex']
-                        if  next_index == limit + 1 or next_index == limit:
-                            break
-                        else:
-                            start_index+=10
-                    else:
-                        break
-                else:
-                    # Update csv with -1 because no data found for keyword
-                    start_index = -1
-                    last_page_data = f'{domain},{today.strftime("%Y-%m-%d %H:%M")},{start_index},{project},{words}\n'
-                    last_page(last_page_data)
-                    break
-                            
-            elif response.status_code == 429:
-                # Quota is out
-                if api_keys:
-                    api_keys.remove(api_keys[0])
-                    pbar.update(1)
-                    if not api_keys: 
-                        last_page_data = f'{domain},{today.strftime("%Y-%m-%d %H:%M")},{start},{project},{words}\n'
-                        last_page(last_page_data)
-                        
-                        # Pause till the next day
-                        next_day = today + datetime.timedelta(days=1)
-                        year,month, day = next_day.strftime("%Y-%m-%d").split('-')
-                        pause.until(datetime.datetime(int(year), int(month), int(day)))
-                        api_keys = get_txt_data('api_keys')
-                        # break
-            elif 'error' in json.loads(response.text):
+        limit = 10
+        today = datetime.datetime.now() #today's date
+        
+        while True:
+            if start == -1:
                 break
-        else:
-            break
 
-    # Keeping track of the extracted keywords and start index
-    last_page_data = f'{domain},{today.strftime("%Y-%m-%d %H:%M")},{start_index},{project},{words}\n'
-    last_page(last_page_data)
+            # page_url = f"https://www.googleapis.com/customsearch/v1?key={api_keys[0]}&start={start_index}&cx={cse_key}&cr={location}&siteSearch={domain}&siteSearchFilter={siteSearchFilter}&exactTerms={required_keywords}&orTerms={or_keywords}&as_occt=body"
+            page_url = f"https://www.googleapis.com/customsearch/v1?key={api_keys[0]}&start={start_index}&cx={cse_key}&siteSearch={domain}&siteSearchFilter={siteSearchFilter}&exactTerms={required_keywords}&orTerms={or_keywords}&as_occt=body"
+            # Dont send request if start_status is "NOT_FOUND"
+            if start_status == 'NOT_FOUND' or start <= limit - 1:
+                response = requests.get(page_url)
+                if response.status_code == 200:
+                    response = json.loads(response.text)
+                    if 'items' in response:
+                        results += response['items']
+
+                        if 'nextPage' in response['queries']:
+                            # Limiting to top 11 results
+                            next_index = response['queries']['nextPage'][0]['startIndex']
+                            if  next_index == limit + 1 or next_index == limit:
+                                break
+                            else:
+                                start_index+=10
+                        else:
+                            break
+                    else:
+                        # Update csv with -1 because no data found for keyword
+                        start_index = -1
+                        last_page_data = f'{domain},{today.strftime("%Y-%m-%d %H:%M")},{start_index},{project},{words}\n'
+                        last_page(last_page_data)
+                        break
+                                
+                elif response.status_code == 429:
+                    # Quota is out
+                    if api_keys:
+                        api_keys.remove(api_keys[0])
+                        pbar.update(1)
+                        if not api_keys: 
+                            last_page_data = f'{domain},{today.strftime("%Y-%m-%d %H:%M")},{start},{project},{words}\n'
+                            last_page(last_page_data)
+                            
+                            # Pause till the next day
+                            next_day = today + datetime.timedelta(days=1)
+                            year,month, day = next_day.strftime("%Y-%m-%d").split('-')
+                            pause.until(datetime.datetime(int(year), int(month), int(day)))
+                            api_keys = get_txt_data('api_keys')
+                            # break
+                elif 'error' in json.loads(response.text):
+                    break
+            else:
+                break
+
+        # Keeping track of the extracted keywords and start index
+        if start_index != -1 or start != -1:
+            last_page_data = f'{domain},{today.strftime("%Y-%m-%d %H:%M")},{start_index},{project},{words}\n'
+            last_page(last_page_data)
+
+        # domains.remove(domain)
 
     return results
 
